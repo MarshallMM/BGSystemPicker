12a02b2d16a82e3c21fbdddd376dec01a82288eb
mars <marshallpmiller@gmail.com>
2024-08-19 12:43:02 -0700

    made the git version a little cleaner

--- build.sh
+++ build.sh
@@ -1,11 +1,5 @@
 # Step 1: Retrieve the Git description with tags
 GIT_DESCRIBE=$(git describe --tags --dirty --always)
 
-# Step 2: Retrieve the abbreviated commit hash
-GIT_COMMIT_HASH=$(git rev-parse --short HEAD)
-
-# Step 3: Combine them, ensuring the hash is always included
-GIT_DESCRIPTION="${GIT_DESCRIBE}-${GIT_COMMIT_HASH}"
-
-# Step 4: Build the Go binary, embedding the Git description
-env GOOS=linux GOARCH=arm GOARM=5 go build -ldflags "-X 'main.gitDescription=${GIT_DESCRIPTION}'"
+# Step 2: Build the Go binary, embedding the Git description
+env GOOS=linux GOARCH=arm GOARM=5 go build -ldflags "-X 'main.gitDescription=${GIT_DESCRIBE}'"
