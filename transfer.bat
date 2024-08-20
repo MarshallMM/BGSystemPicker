929557968b3b44fed41027ce393589ce0f2e25e1
mars <marshallpmiller@gmail.com>
2024-08-20 16:32:17 -0700

    update to use keys and automatically stop service

--- transfer.bat
+++ transfer.bat
@@ -2,6 +2,7 @@
 cd C:\Users\marsh\BGSystemPicker\src >NUL
 C:\Users\marsh\BGSystemPicker\build.sh >NUL
 ECHO Remember to stop service
+echo.| plink.exe -t -i C:\Users\marsh\.ssh\puttykey.ppk pi@192.168.1.77 "sudo systemctl stop systemBot.service"
 scp BGSystemPicker pi@192.168.1.77:/home/pi/systemBot
-pause
+echo.| plink.exe -t -i C:\Users\marsh\.ssh\puttykey.ppk pi@192.168.1.77 "sudo systemctl start systemBot.service"
 del BGSystemPicker
